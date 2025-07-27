4a0f3a45f869c2e961072ba09dcab5dc074ddb35
Jamie-Black <154505609+Jamie-Black@users.noreply.github.com>
2025-07-27 18:46:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  hamburger.classList.toggle('toggle');
+});
+
+// Scroll-to-Top functionality
+const scrollBtn = document.getElementById('scrollToTopBtn');
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 200) {
+    scrollBtn.style.display = 'block';
+  } else {
+    scrollBtn.style.display = 'none';
+  }
+});
+
+scrollBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
